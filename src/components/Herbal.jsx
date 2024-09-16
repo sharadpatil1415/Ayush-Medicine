cebe467d6f4cad578025e4ccbd78ab471137931e
shreyashap <107754587+shreyashap@users.noreply.github.com>
2024-09-16 13:21:00 +0530

    Font change

--- src/components/Herbal.jsx
+++ src/components/Herbal.jsx
@@ -34,7 +34,7 @@ const Herbal = () => {
       </nav>
       <div className="flex flex-col justify-center items-center">
         <h2 className="text-4xl font-bold text-center mb-10 text-green-400 z-20">
-          Most Popular Ayush Plants
+          Most Popular AYUSH Plants
         </h2>
         <div className="w-[80%] grid grid-cols-1 md:grid-cols-3 gap-10">
           <Card
@@ -63,7 +63,7 @@ const Herbal = () => {
       </div>
       <div className="flex flex-col justify-center items-center">
         <h2 className="text-4xl font-bold text-center my-14 text-green-400 z-20">
-          Most Rare Ayush Plants
+          Most Rare AYUSH Plants
         </h2>
         <div className="w-[80%] grid grid-cols-1 md:grid-cols-3 gap-10">
           <Card
